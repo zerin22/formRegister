23b6c9a6cdd1cbe9dc3066b3badc6ca322dd4853
zerin22 <tajrin.022@gmail.com>
2023-08-08 23:50:35 +0600

    update

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import './assets/main.css'
+import './assets/tailwind.css'
+import axios from 'axios'
 
-createApp(App).mount('#app')
+
+axios.defaults.baseURL = 'http://127.0.0.1:8000/api/v1';
+axios.defaults.headers['Authorization'] = `Bearer ${localStorage.getItem('token')}`;
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
